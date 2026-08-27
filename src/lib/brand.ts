e675e85ef8b059a53ce189ddfbd6271d41195869
Emre Gundogdu <emre.gundogdu016@gmail.com>
2026-08-27 20:44:56 +0300

    style: Update katilim_finans_sadece_logo SVG file from Downloads and add cache buster

--- src/lib/brand.ts
+++ src/lib/brand.ts
@@ -1,5 +1,5 @@
 export const BRAND_NAME = "KatılımFinans";
 export const BRAND_TAGLINE = "Sakin finans uzmanı";
-export const BRAND_LOGO = "/logos/katilim_finans_sadece_logo.svg";
+export const BRAND_LOGO = "/logos/katilim_finans_sadece_logo.svg?v=2";
 /** AI chatbot / asistan maskotu (penguen) */
 export const ASSISTANT_MASCOT = "/logos/penguen-maskot-seffaf.svg?v=1";
